fc3c68942d7353a7f871a6c13fcdb5eb4cae208d
Marinarium <schwmk@gmail.com>
2020-10-06 21:31:17 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let arr = str.split('');
+  let reverseArr = arr.reverse();
+  let reverseStr = reverseArr.join('');
+  let reverseN = +reverseStr;
+  return reverseN;
 }
